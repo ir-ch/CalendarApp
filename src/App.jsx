3f0c88c631768d81575c51b78ab71cd74c192a2d
ir-ch <ir-ch@gmail.com>
2025-08-06 16:51:04 +0100

    add CalendarApp component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import СalendarApp from './components/СalendarApp'
+import './components/СalendarApp.css'
+
 const App = () => {
   return (
     <div className='container'>
+      <СalendarApp />
     </div>
   )
 }
