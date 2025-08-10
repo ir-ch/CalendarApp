55a4c8c2144d5034006b17fb1457007c05bc0348
ir-ch <ir-ch@gmail.com>
2025-08-10 16:32:08 +0100

    add functionality of event editing and event deleting

--- src/components/CalendarApp.jsx
+++ src/components/CalendarApp.jsx
@@ -12,6 +12,7 @@ const СalendarApp = () => {
     const [events, setEvents] = useState([]);
     const [eventTime, setEventTime] = useState({ hours: '00', minutes: '00' });
     const [eventText, setEventText] = useState('');
+    const [editingEvent, setEditingEvent] = useState(null);
 
     const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
     const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
@@ -35,6 +36,7 @@ const СalendarApp = () => {
             setShowEventPopup(true);
             setEventTime({ hours: '00', minutes: '00' });
             setEventText("");
+            setEditingEvent(null);
         }
     }
 
@@ -48,15 +50,48 @@ const СalendarApp = () => {
 
     const handleEventSubmit = () => {
         const newEvent = {
+            id: editingEvent ? editingEvent.id : Date.now(),
             date: selectedDate,
             time: `${eventTime.hours.padStart(2, '0')}:${eventTime.minutes.padStart(2, '0')}`,
             text: eventText
         }
 
-        setEvents([...events, newEvent]);
+        let updatedEvents = [...events];
+
+        if (editingEvent) {
+            updatedEvents = updatedEvents.map((event) => event.id === editingEvent.id ? newEvent : event)
+        } else {
+            updatedEvents.push(newEvent);
+        }
+
+        updatedEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+        setEvents(updatedEvents);
         setEventTime({ hours: '00', minutes: '00' });
         setEventText("");
         setShowEventPopup(false);
+        setEditingEvent(null);
+    }
+
+    const handleEditEvent = (event) => {
+        setSelectedDate(new Date(event.date));
+        setEventTime({
+            hours: event.time.split(":")[0],
+            minutes: event.time.split(":")[1]
+        });
+        setEventText(event.text);
+        setEditingEvent(event);
+        setShowEventPopup(true);
+    }
+
+    const handleDeleteEvent = (eventId) => {
+        const updatedEvents = events.filter(event => event.id !== eventId)
+        setEvents(updatedEvents);
+    }
+
+    const handleTimeChange = (e) => {
+        const { name, value } = e.target;
+        setEventTime((prevTime) => ({ ...prevTime, [name]: value.padStart(2, '0') }))
     }
 
     return (
@@ -95,10 +130,10 @@ const СalendarApp = () => {
                             <div className="event-popup-time">Time</div>
                             <input type="number" name="hours" min={0} max={24} className="hours"
                                 value={eventTime.hours}
-                                onChange={(e) => setEventTime({ ...eventTime, hours: e.target.value })} />
+                                onChange={handleTimeChange} />
                             <input type="number" name="minutes" min={0} max={60} className="minutes"
                                 value={eventTime.minutes}
-                                onChange={(e) => setEventTime({ ...eventTime, minutes: e.target.value })} />
+                                onChange={handleTimeChange} />
                         </div>
                         <textarea placeholder="Enter event text (maximum 60 characters)"
                             value={eventText}
@@ -107,7 +142,7 @@ const СalendarApp = () => {
                                     setEventText(e.target.value)
                                 }
                             }}></textarea>
-                        <button className="event-popup-btn" onClick={handleEventSubmit}>Add event</button>
+                        <button className="event-popup-btn" onClick={handleEventSubmit}>{editingEvent ? "Update event" : "Add event"}</button>
                         <button className="close-event-popup" onClick={() => setShowEventPopup(false)}>
                             <i className="bx bx-x"></i>
                         </button>
@@ -120,8 +155,8 @@ const СalendarApp = () => {
                         </div>
                         <div className="event-text">{event.text}</div>
                         <div className="event-buttons">
-                            <i className="bx bxs-edit-alt"></i>
-                            <i className="bx bxs-message-alt-x"></i>
+                            <i className="bx bxs-edit-alt" onClick={() => handleEditEvent(event)}></i>
+                            <i className="bx bxs-message-alt-x" onClick={() => handleDeleteEvent(event.id)}></i>
                         </div>
                     </div>
                 ))}
